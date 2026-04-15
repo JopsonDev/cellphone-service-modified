af82a5f90ca9d6a23c4a8b5de14d7e42a84f2835
JopsonDev <jopsonmason@outlook.com>
2026-04-15 11:21:03 -0400

    did housekeeping and finished exercise

--- src/main/java/com/pluarlsight/CellPhoneApplication.java
+++ src/main/java/com/pluarlsight/CellPhoneApplication.java
@@ -14,7 +14,8 @@ public class CellPhoneApplication {
         display(phone1);
         display(phone2);
 
-        System.out.println(phone1.getSerialNumber() + "\n" + phone1.getModel() + "\n" + phone1.getCarrier() + "\n" + phone1.getPhoneNumber() + "\n" + phone1.getOwner());
+        phone1.dial(phone1.getOwner(), phone2.getPhoneNumber());
+        phone2.dial(phone2.getOwner(), phone1.getPhoneNumber());
 
         System.out.println("Please enter the number you want to call: ");
         String callNumber = scanner.nextLine();
@@ -23,9 +24,10 @@ public class CellPhoneApplication {
         scanner.close();
     }
     public static void display(CellPhone phone){
-        System.out.println(phone.getSerialNumber() + "\n" + phone.getModel() + "\n" + phone.getCarrier() + "\n" + phone.getPhoneNumber() + "\n" + phone.getOwner());
+        System.out.println("Phone Information:\n" + phone.getSerialNumber() + "\n" + phone.getModel() + "\n" + phone.getCarrier() + "\n" + phone.getPhoneNumber() + "\n" + phone.getOwner() + "\n");
     }
-    public static CellPhone newPhone(Scanner scanner, CellPhone phone) {
+    public static void newPhone(Scanner scanner, CellPhone phone) {
+        System.out.println("Please enter phone information:");
         System.out.print("Serial Number: ");
         int serialNumber = scanner.nextInt();
         scanner.nextLine();
@@ -44,6 +46,5 @@ public class CellPhoneApplication {
         phone.setPhoneNumber(phoneNumber);
         phone.setOwner(owner);
 
-        return phone;
     }
 }
